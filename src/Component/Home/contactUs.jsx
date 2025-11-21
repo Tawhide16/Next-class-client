2fe63aa1b888c4e01dab38fd2e9b8c7e50c8a782
Tawhide16 <tawhideh.b10@gmail.com>
2025-11-21 14:45:47 +0600

    modify the contact us section

--- src/Component/Home/contactUs.jsx
+++ src/Component/Home/contactUs.jsx
@@ -23,7 +23,7 @@ const ContactUs = () => {
         </motion.div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-         
+          {/* Contact Information */}
           <motion.div
             initial={{ opacity: 0, x: -20 }}
             whileInView={{ opacity: 1, x: 0 }}
